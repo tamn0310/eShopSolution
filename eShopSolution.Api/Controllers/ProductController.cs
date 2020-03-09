5fb20d605eea0fe8da7bcb70b438cd7c6a799096
tamn0310 <tamn0310@gmail.com>
2020-03-09 19:34:58 +0700

    [commit]: add swagger versioning for project api

--- eShopSolution.Api/Controllers/ProductController.cs
+++ eShopSolution.Api/Controllers/ProductController.cs
@@ -1,25 +1,46 @@
 ﻿using eShopSolution.Application.Catalog.Product;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Threading.Tasks;
 
 namespace eShopSolution.Api.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}")]
+    [Produces("application/json")]
     public class ProductController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
+        private readonly ILogger<ProductController> _logger;
 
-        public ProductController(IPublicProductService publicProductService)
+        public ProductController(IPublicProductService publicProductService, ILogger<ProductController> logger)
         {
             this._publicProductService = publicProductService;
+            this._logger = logger;
         }
 
-        [HttpGet]
+        /// <summary>
+        /// Lấy ra tất cả sản phẩm
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="200">Trả ra tất cả bản ghi</response>
+        /// <response code="400"></response>
+        /// <response code="500">Lỗi server</response>
+        [HttpGet("products")]
         public async Task<IActionResult> Get()
         {
-            var products = await _publicProductService.GetAll();
-            return Ok(products);
+            try
+            {
+                var products = await _publicProductService.GetAll();
+                return Ok(products);
+            }
+            catch (Exception e)
+            {
+                this._logger.LogError(e.Message, e);
+                throw e;
+            }
         }
     }
 }
--- eShopSolution.Api/Startup.cs
+++ eShopSolution.Api/Startup.cs
@@ -1,23 +1,36 @@
+using eShopSolution.Api.SwaggerOptions;
 using eShopSolution.Application.Catalog.Product;
 using eShopSolution.Data.EF;
 using eShopSolution.Utilities.Contants;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace eShopSolution.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Environment = environment;
+            this.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -29,10 +42,66 @@ namespace eShopSolution.Api
             services.AddTransient<IPublicProductService, PublicProductService>();
 
             services.AddControllers();
+
+            //api versioning
+            services.AddApiVersioning(
+                config =>
+                {
+                    config.ReportApiVersions = true;
+                    config.AssumeDefaultVersionWhenUnspecified = true;
+                    config.DefaultApiVersion = new ApiVersion(1, 0);
+                    // config.ApiVersionReader = new HeaderApiVersionReader("api-version");
+                });
+
+            services.AddVersionedApiExplorer(
+                options =>
+                {
+                    options.GroupNameFormat = "'v'VVV";
+
+                    // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                    // can also be used to control the format of the API version in route templates
+                    options.SubstituteApiVersionInUrl = true;
+                });
+
+            // Enable authentication JWT
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using token",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    BearerFormat = "JWT"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id = "Bearer", //The name of the previously defined security scheme.
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        }, new List<string>()
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+                              IApiVersionDescriptionProvider provider, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -41,7 +110,26 @@ namespace eShopSolution.Api
 
             app.UseHttpsRedirection();
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            app.UseSwaggerUI(
+                options =>
+                {
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint(
+                            $"/swagger/{description.GroupName}/swagger.json",
+                            description.GroupName.ToUpperInvariant());
+                    }
+
+                    options.OAuthClientId(this.Configuration.GetSection("Authentication:ClientId").Value);
+                    options.OAuthClientSecret(this.Configuration.GetSection("Authentication:Secret").Value);
+                });
+
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
